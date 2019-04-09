ac88f121a692a05be24d07a7734f33e884c57d9f
HelloVansal <959661369@qq.com>
2019-04-09 15:52:35 +0800

    update

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,64 +1,69 @@
 <template>
   <div class="goods">
-    <div class="menu-wrap" ref="menuWrap">
-      <ul class="menu">
-        <li
-          class="menu-item border-1px"
-          v-for="(item, index) of goods"
-          :key="index"
-          :class="{'current':currentIndex === index}"
-          @click="foodMove(index,$event)"
-        >
-          <span class="text">
-            <span v-if="item.type>0" class="icon" :class="classMap[item.type]"></span>
-            <span>{{item.name}}</span>
-          </span>
-        </li>
-      </ul>
-    </div>
-    <div class="main-wrap" ref="mainWrap">
-      <div>
-        <ul class="main food-list-hook" v-for="(good, index) of goods" :key="index">
-          <div class="title-wrap">
-            <div class="title">{{good.name}}</div>
-          </div>
-          <ul class="foods-wrap">
-            <li class="food-item border-1px" v-for="(item, index) of good.foods" :key="index">
-              <div class="food-img-wrap">
-                <img :src="item.image" width="57" height="57">
-              </div>
-              <div class="food-content">
-                <div class="name">{{item.name}}</div>
-                <div class="desc" v-if="item.description">{{item.description}}</div>
-                <div class="desc2">
-                  <span class="sellCount">月售{{item.sellCount}}份</span>
-                  <span class="rating">好评率{{item.rating}}%</span>
-                </div>
-                <div class="price-all">
-                  <span class="price-icon">￥</span>
-                  <span class="price">{{item.price}}</span>
-                  <span class="old-price" v-if="item.oldPrice">￥{{item.oldPrice}}</span>
-                </div>
-                <div class="add">
-                  <span class="iconfont" v-show="false">&#xe60c;</span>
-                  <span class="num" v-show="false">0</span>
-                  <span class="iconfont">&#xe60b;</span>
-                </div>
-              </div>
-            </li>
-          </ul>
+    <div class="goods-wrap">
+      <div class="menu-wrap" ref="menuWrap">
+        <ul class="menu">
+          <li
+            class="menu-item border-1px"
+            v-for="(item, index) of goods"
+            :key="index"
+            :class="{'current':currentIndex === index}"
+            @click="foodMove(index,$event)"
+          >
+            <span class="text">
+              <span v-if="item.type>0" class="icon" :class="classMap[item.type]"></span>
+              <span>{{item.name}}</span>
+            </span>
+          </li>
         </ul>
       </div>
+      <div class="main-wrap" ref="mainWrap">
+        <div>
+          <ul class="main food-list-hook" v-for="(good, index) of goods" :key="index">
+            <div class="title">{{good.name}}</div>
+            <ul class="foods-wrap">
+              <li class="food-item border-1px" v-for="(item, index) of good.foods" :key="index">
+                <div class="food-img-wrap">
+                  <img :src="item.image" width="60" height="60">
+                </div>
+                <div class="food-content">
+                  <div class="name">{{item.name}}</div>
+                  <div class="desc" v-if="item.description">{{item.description}}</div>
+                  <div class="desc2">
+                    <span class="sellCount">月售{{item.sellCount}}份</span>
+                    <span class="rating">好评率{{item.rating}}%</span>
+                  </div>
+                  <div class="price-all">
+                    <span class="price-icon">￥</span>
+                    <span class="price">{{item.price}}</span>
+                    <span class="old-price" v-if="item.oldPrice">￥{{item.oldPrice}}</span>
+                  </div>
+                  <div class="add">
+                    <span class="iconfont" v-show="false">&#xe60c;</span>
+                    <span class="num" v-show="false">0</span>
+                    <span class="iconfont">&#xe60b;</span>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </ul>
+        </div>
+      </div>
     </div>
+    <shoppingCart class="shoppingCart"></shoppingCart>
   </div>
 </template>
 
 <script>
 import BScroll from 'better-scroll'
+import shoppingCart from 'components/shoppingCart/shoppingCart'
 
 const ERR_OK = 0
 
 export default {
+  components: {
+    shoppingCart
+  },
   data () {
     return {
       goods: {},
@@ -138,138 +143,134 @@ export default {
 <style lang='stylus' scoped>
 @import '../../common/stylus/mixin'
 .goods
-  display flex
-  position absolute
-  width 100%
-  top 174px
-  left 0
-  bottom 47px
-  overflow hidden
-  .menu-wrap
+  /*position relative*/
+  .goods-wrap
     display flex
-    flex-shrink 0
-    flex-direction column
-    width 80px
-    background-color #f3f5f7
-    .current
-      z-index 10
-      margin-top -1px
-      font-weight 700
-      background-color #fff
-      &.text
-        border-none()
-    .menu-item
-      display table
-      width 100%
-      padding 0 12px
-      box-sizing border-box
-      height 54px
-      .text
-        display table-cell
-        vertical-align middle
-        font-size 12px
-        line-height 14px
-        color rgb(24, 20, 20)
-        border-1px(rgba(7, 17, 27, 0.1))
-      .icon
-        display inline-block
-        width 14px
-        height 14px
-        font-size 14px
-        vertical-align top
-        background-size 14px 14px
-        background-repeat no-repeat
-        &.decrease
-          bg-img decrease_3
-        &.discount
-          bg-img discount_3
-        &.guarantee
-          bg-img guarantee_3
-        &.special
-          bg-img special_3
-        &.invoice
-          bg-img invoice_3
-  .main-wrap
-    flex 1
+    position absolute
     width 100%
-    .main
-      .title-wrap
-        padding-left 3px
-        background-color #d9dde1
+    top 174px
+    left 0
+    bottom 47px
+    overflow hidden
+    .menu-wrap
+      display flex
+      flex-shrink 0
+      flex-direction column
+      width 80px
+      background-color #f3f5f7
+      .current
+        z-index 10
+        margin-top -1px
+        font-weight 700
+        background-color #fff
+        &.text
+          border-none()
+      .menu-item
+        display table
+        width 100%
+        padding 0 12px
+        box-sizing border-box
+        height 54px
+        .text
+          display table-cell
+          vertical-align middle
+          font-size 12px
+          line-height 14px
+          color rgb(24, 20, 20)
+          border-1px(rgba(7, 17, 27, 0.1))
+        .icon
+          display inline-block
+          width 14px
+          height 14px
+          font-size 14px
+          vertical-align top
+          background-size 14px 14px
+          background-repeat no-repeat
+          &.decrease
+            bg-img decrease_3
+          &.discount
+            bg-img discount_3
+          &.guarantee
+            bg-img guarantee_3
+          &.special
+            bg-img special_3
+          &.invoice
+            bg-img invoice_3
+    .main-wrap
+      flex 1
+      width 100%
+      .main
         .title
           width 100%
           height 26px
-          padding-left 11px
+          padding-left 8px
           font-size 12px
           line-height 26px
+          border-left 3px solid #d9dde1
           color rgb(147, 153, 159)
           background-color #f3f5f7
-      .foods-wrap
-        width 100%
-        box-sizing border-box
-        padding 0 18px
-        .food-item
-          display flex
-          position relative
-          padding 18px 0
+        .foods-wrap
+          width 100%
           box-sizing border-box
-          border-1px rgba(7, 17, 27, 0.1)
-          &:last-child
-            border-none()
-          .food-img-wrap
-            display inline-block
-            margin-right 10px
-            vertical-align top
-          .food-content
-            flex 1
-            min-width 180px
-            margin-top 2px
-            .name
-              font-size 14px
-              color rgb(7, 17, 27)
-              height 14px
-              line-height 14px
-              margin-bottom 8px
-            .desc, .desc2
-              font-size 10px
-              color rgb(147, 153, 159)
-              width 80%
-              margin-bottom 8px
-              overflow hidden
-              white-space nowrap
-              text-overflow ellipsis
-              .rating
-                margin-left 12px
-            .price-all
-              font-size 0
-              .price-icon, .price, .old-price
+          padding 0 18px
+          .food-item
+            display flex
+            position relative
+            padding 18px 0
+            box-sizing border-box
+            border-1px rgba(7, 17, 27, 0.1)
+            &:last-child
+              border-none()
+            .food-img-wrap
+              display inline-block
+              margin-right 10px
+              vertical-align top
+            .food-content
+              flex 1
+              margin-top 2px
+              .name
+                font-size 14px
+                color rgb(7, 17, 27)
+                line-height 14px
+                margin-bottom 8px
+              .desc, .desc2
                 font-size 10px
                 color rgb(147, 153, 159)
-                font-weight normal
+                margin-bottom 8px
+               .desc
+                 line-height 14px
+              .rating
+                  margin-left 6px
+              .price-all
+                font-size 0
+                .price-icon, .price, .old-price
+                  font-size 10px
+                  color rgb(147, 153, 159)
+                  font-weight normal
+                  line-height 24px
+                .price-icon
+                  color red
+                .price
+                  font-size 14px
+                  font-weight 700
+                  color red
+                .old-price
+                  margin-left 8px
+                  text-decoration line-through
+              .add
+                position absolute
+                right 0
+                bottom 18px
+                font-size 0
+              .num
+                display inline-block
+                width 24px
+                font-size 10px
                 line-height 24px
-              .price-icon
-                color red
-              .price
-                font-size 14px
-                font-weight 700
-                color red
-              .old-price
-                margin-left 8px
-                text-decoration line-through
-            .add
-              position absolute
-              right 0
-              bottom 18px
-              font-size 0
-            .num
-              display inline-block
-              width 24px
-              font-size 10px
-              line-height 24px
-              text-align center
-              color rgb(147, 153, 159)
-            .iconfont
-              vertical-align top
-              font-size 20px
-              line-height 24px
+                text-align center
+                color rgb(147, 153, 159)
+              .iconfont
+                vertical-align top
+                font-size 20px
+                line-height 24px
 </style>
---
+++ src/components/shoppingCart/shoppingCart.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="shoppingCart">
+    <div class="shoppingCartImg-wrap">
+      <div class="shoppingCartImg"></div>
+    </div>
+    <div class="delivery-fee"></div>
+    <div class="Start-fee"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'shoppingCart'
+}
+</script>
+
+<style lang="stylus" scoped>
+.shoppingCart
+  position absolute
+  left 0
+  right 0
+  bottom 0
+  height 47px
+  background-color #141d27
+</style>
